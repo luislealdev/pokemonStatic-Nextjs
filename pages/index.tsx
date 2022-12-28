e322aefcfb3ede5a9f757b467e8252dd687b7a57
Luis Leal <luisrrleal@gmail.com>
2022-12-27 23:50:39 -0600

    Add next ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@nextui-org/react";
+
 export default function HomePage() {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <Button color="gradient">Hola Mundo</Button>
     </>
   )
 }
