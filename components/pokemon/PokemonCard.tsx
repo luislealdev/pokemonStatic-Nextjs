a4fc812717d12bd4c042212be2332814405078c5
Luis Leal <luisrrleal@gmail.com>
2022-12-29 23:21:15 -0600

    Create pokemon card

---
+++ components/pokemon/PokemonCard.tsx
@@ -0,0 +1,31 @@
+import { smallPokemon } from '../../interfaces';
+import { FC } from 'react';
+import { Card, Grid, Row, Text } from "@nextui-org/react"
+
+interface Props {
+    pokemon: smallPokemon
+}
+export const PokemonCard: FC<Props> = ({ pokemon }) => {
+    return (
+        <Grid xs={6} sm={3} key={pokemon.id}>
+            <Card isPressable>
+                <Card.Body css={{ p: 0 }}>
+                    <Card.Image
+                        src={pokemon.img}
+                        objectFit="cover"
+                        height={140}
+                        alt={pokemon.name}
+                    />
+                </Card.Body>
+                <Card.Footer css={{ justifyItems: "flex-start" }}>
+                    <Row wrap="wrap" justify="space-between" align="center">
+                        <Text b transform="uppercase">{pokemon.name}</Text>
+                        <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
+                            #{pokemon.id}
+                        </Text>
+                    </Row>
+                </Card.Footer>
+            </Card>
+        </Grid>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,24 @@
-import { Card, Grid, Row, Text } from "@nextui-org/react"
-import { NextPage } from "next"
 import { Layout } from "../components/layouts"
+import { PokemonCard } from "../components/pokemon"
+import { pokeApi } from "../api";
+import { pokemonsList, smallPokemon } from "../interfaces";
+
+import { Grid } from "@nextui-org/react"
+
+import { NextPage, GetStaticProps } from "next"
+
+
 
 interface Props {
   pokemons: smallPokemon[]
 }
 const HomePage: NextPage<Props> = ({ pokemons }) => {
 
-  console.log({ pokemons });
-
-
   return (
     <Layout title={"Luis"}>
       <Grid.Container gap={2} justify="flex-start">
-        {pokemons.map(({ id, name, img }: smallPokemon) => {
-          return <Grid xs={6} sm={3} key={id}>
-            <Card isPressable>
-              <Card.Body css={{ p: 0 }}>
-                <Card.Image
-                  src={img}
-                  objectFit="cover"
-                  height={140}
-                  alt={name}
-                />
-              </Card.Body>
-              <Card.Footer css={{ justifyItems: "flex-start" }}>
-                <Row wrap="wrap" justify="space-between" align="center">
-                  <Text b>{name}</Text>
-                  <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-                    {id}
-                  </Text>
-                </Row>
-              </Card.Footer>
-            </Card>
-          </Grid>
+        {pokemons.map((pokemon: smallPokemon) => {
+          return <PokemonCard pokemon={pokemon} />
         })}
       </Grid.Container>
 
@@ -46,10 +31,7 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
 //- The data comes from a headless CMS.
 //- The data can be publicly cached (not user-specific).
 //- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
-import { GetStaticProps } from 'next'
-import { pokeApi } from "../api";
-import { pokemonsList, smallPokemon } from "../interfaces";
-import Image from "next/image"
+
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<pokemonsList>("/pokemon?limit=151&offset=0");
