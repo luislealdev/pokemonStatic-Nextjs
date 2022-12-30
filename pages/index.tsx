553d450f0c268035a26cb95e097bb247e7247258
Luis Leal <luisrrleal@gmail.com>
2022-12-29 21:06:02 -0600

    Add axios and configurate pokeApi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
-import { Button } from "@nextui-org/react"
+import { Text } from "@nextui-org/react"
 import { NextPage } from "next"
 import { Layout } from "../components/layouts"
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
+
   return (
     <Layout title={"Luis"}>
-      <Button color="gradient">
-        Hola Mundo
-      </Button>
+      <Text>Pokemon 1</Text>
     </Layout>
   )
+}
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+import { GetStaticProps } from 'next'
+import { pokeApi } from "../api";
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get("/pokemon?limit=151&offset=0");
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
 }
 
 export default HomePage;
