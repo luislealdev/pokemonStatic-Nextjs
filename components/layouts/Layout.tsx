6b7a68b1827d5603fa62d3b3351171551595fbf1
Luis Leal <luisrrleal@gmail.com>
2022-12-29 20:25:06 -0600

    Add navbar

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,26 +1,29 @@
 import { FC } from 'react';
 import Head from 'next/head';
+import { Navbar } from '../ui/Navbar';
 
 interface Props {
-    children?: JSX.Element,
-    title: string
-  };
+  children?: JSX.Element,
+  title: string
+};
 
-export const Layout: FC<Props>= ({children, title}) => {
+export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
-    <Head>
+      <Head>
         <title>
           {title || 'PokemonApp'}
         </title>
-        <meta name='author' content='Luis Leal'/>
-        <meta name='description' content={`Información sobre el pokémon ${title}`}/>
-        <meta name='keywords' content={`${title}, pokemon, pokedex`}/>
-    </Head>
+        <meta name='author' content='Luis Leal' />
+        <meta name='description' content={`Información sobre el pokémon ${title}`} />
+        <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+      </Head>
 
-    <main>
+      <Navbar />
+
+      <main style={{ padding: '0 20px' }}>
         {children}
-    </main>
+      </main>
 
     </>
   )
